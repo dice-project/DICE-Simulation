bf2b0ff6c02682d833328bb3e203abac4f9a7140
Abel Gómez <abel.gomez@unizar.es>
2016-05-12 18:50:18 +0200

    Add UiConstants class

---
+++ es.unizar.disco.core.ui/src/es/unizar/disco/core/ui/util/UiConstants.java
@@ -0,0 +1,23 @@
+package es.unizar.disco.core.ui.util;
+
+import org.eclipse.swt.layout.GridLayout;
+
+public final class UiConstants {
+	
+	private static final GridLayout GRID_LAYOUT = new GridLayout();
+
+	public static final int DEFAULT_MARGIN = GRID_LAYOUT.marginWidth;
+
+	public static final int DEFAULT_MARGIN_WIDTH = GRID_LAYOUT.marginWidth;
+
+	public static final int DEFAULT_MARGIN_HEIGHT = GRID_LAYOUT.marginWidth;
+
+	public static final int DEFAULT_SPACING = GRID_LAYOUT.horizontalSpacing;
+
+	public static final int DEFAULT_HORIZONTAL_SPACING = GRID_LAYOUT.horizontalSpacing;
+
+	public static final int DEFAULT_VERTICAL_SPACING = GRID_LAYOUT.verticalSpacing;
+	
+	private UiConstants() {
+	}
+}
