e17fae428913edfcae61c3ff93b02c743cba10b7
Abel Gómez <abel.gomez@unizar.es>
2016-07-04 18:18:22 +0200

    Fix broken links in traceability models

--- es.unizar.disco.simulation.models/src/es/unizar/disco/simulation/models/builders/IAnalyzableModelBuilder.java
+++ es.unizar.disco.simulation.models/src/es/unizar/disco/simulation/models/builders/IAnalyzableModelBuilder.java
@@ -3,38 +3,57 @@ package es.unizar.disco.simulation.models.builders;
 import java.util.List;
 
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.uml2.uml.Element;
 
 import es.unizar.disco.simulation.models.datatypes.PrimitiveVariableAssignment;
 import es.unizar.disco.simulation.models.traces.TraceSet;
 
 public interface IAnalyzableModelBuilder {
-	
+
 	public final class ModelResult {
-		
+
 		private final List<EObject> model;
-		
+
 		private final TraceSet traceSet;
 
 		private final IStatus status;
-		
+
 		public ModelResult(List<EObject> list, TraceSet traceSet, IStatus status) {
 			this.model = list;
 			this.traceSet = traceSet;
 			this.status = status;
 		}
-		
+
+		/**
+		 * Returns the result model. This method returns the original model
+		 * produced by the transformation and not a shallow copy since it is
+		 * required to maintain the proper traceability links. Clients should
+		 * not modify the model obtained by calling this method.
+		 * 
+		 * @return the produced model
+		 */
 		public List<EObject> getModel() {
-			return new BasicEList<>(EcoreUtil.copyAll(model));
+			return model;
 		}
-		
+
+		/**
+		 * Returns the trace model. This method returns the original trace model
+		 * produced by the transformation and not a shallow copy. This is needed
+		 * to maintain the proper traceability links. Clients should not modify
+		 * the model obtained by calling this method.
+		 * 
+		 * @return the trace model
+		 */
 		public TraceSet getTraceSet() {
-			return EcoreUtil.copy(traceSet);
+			return traceSet;
 		}
-		
+
+		/**
+		 * Returns the status of the executed {@link IAnalyzableModelBuilder}
+		 * 
+		 * @return the {@link IStatus}
+		 */
 		public IStatus getStatus() {
 			return status;
 		}
