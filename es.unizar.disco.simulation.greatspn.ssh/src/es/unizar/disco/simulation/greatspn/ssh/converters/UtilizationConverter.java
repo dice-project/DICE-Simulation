b94267212a1ec5487020823490599d55b699152b
Abel Gómez <abel.gomez@unizar.es>
2016-07-05 21:57:56 +0200

    Move supported measures declaration to simulator plugin

---
+++ es.unizar.disco.simulation.greatspn.ssh/src/es/unizar/disco/simulation/greatspn/ssh/converters/UtilizationConverter.java
@@ -0,0 +1,14 @@
+package es.unizar.disco.simulation.greatspn.ssh.converters;
+
+import es.unizar.disco.simulation.models.measures.DomainMeasure;
+import es.unizar.disco.simulation.models.measures.MeasureConverter;
+import es.unizar.disco.simulation.models.toolresult.AnalyzableElementInfo;
+
+public class UtilizationConverter implements MeasureConverter {
+
+	@Override
+	public DomainMeasure convert(AnalyzableElementInfo info) {
+		return null;
+	}
+
+}
