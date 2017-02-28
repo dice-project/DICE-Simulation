38284146189e5fda238221548f6ba54d551e613f
Diego <diegop@unizar.es>
2017-02-28 17:47:13 +0100

    Create structure for Reliability evaluation in Storm

--- bundles/es.unizar.disco.pnml.m2m/src/es/unizar/disco/pnml/m2m/PnmlM2mPlugin.java
+++ bundles/es.unizar.disco.pnml.m2m/src/es/unizar/disco/pnml/m2m/PnmlM2mPlugin.java
@@ -17,6 +17,9 @@ public class PnmlM2mPlugin implements BundleActivator {
 	
 	public static final String DTSM_HADOOP_AD2PNML_TRANSFORMATION_URI = "platform:/plugin/" + PLUGIN_ID + "/transformations/dtsm-hadoop-ad2pnml.qvto"; //$NON-NLS-1$ //$NON-NLS-2$
 	
+	public static final String DTSM_STORM_AD2PNML_RELIABILITY_TRANSFORMATION_URI = "platform:/plugin/" + PLUGIN_ID + "/transformations/dtsm-storm-ad2pnml-reliability.qvto"; //$NON-NLS-1$ //$NON-NLS-2$
+	
+	
 	private static BundleContext context;
 
 	static BundleContext getContext() {
---
+++ bundles/es.unizar.disco.pnml.m2m/src/es/unizar/disco/pnml/m2m/builder/StormScenario2PnmlReliabilityResourceBuilder.java
@@ -0,0 +1,18 @@
+package es.unizar.disco.pnml.m2m.builder;
+
+import org.eclipse.emf.common.util.URI;
+
+import es.unizar.disco.pnml.m2m.PnmlM2mPlugin;
+import es.unizar.disco.simulation.models.builders.IAnalyzableModelBuilder;
+
+/**
+ * This class will deal with the transformation of an AD to PNML for PERFORMANCE evaluation. The name is not yet updated to avoid touching extension and extesion points in the plugin.xmls
+ * TODO: change the name of the class
+ */
+public class StormScenario2PnmlReliabilityResourceBuilder extends AbstractPnmlResourceBuilder implements IAnalyzableModelBuilder {
+
+	@Override
+	protected URI getTransformationUri() {
+		return URI.createURI(PnmlM2mPlugin.DTSM_STORM_AD2PNML_RELIABILITY_TRANSFORMATION_URI);
+	}
+}
