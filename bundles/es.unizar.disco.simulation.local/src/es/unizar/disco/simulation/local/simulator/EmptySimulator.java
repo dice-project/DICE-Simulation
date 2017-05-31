e9a160b35be315700b4fa1e63584f251c9289e92
Diego <diegop@unizar.es>
2017-05-31 15:33:58 +0200

    fix the i/o of the empty process for local simulation and the result

--- bundles/es.unizar.disco.simulation.local/src/es/unizar/disco/simulation/local/simulator/EmptySimulator.java
+++ bundles/es.unizar.disco.simulation.local/src/es/unizar/disco/simulation/local/simulator/EmptySimulator.java
@@ -1,15 +1,18 @@
 package es.unizar.disco.simulation.local.simulator;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.io.IOUtils;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.ecore.EObject;
 
 import es.unizar.disco.simulation.models.toolresult.ToolResult;
 import es.unizar.disco.simulation.models.traces.TraceSet;
+import es.unizar.disco.simulation.models.wnsim.WnsimFactory;
 import es.unizar.disco.simulation.simulators.ISimulator;
 import es.unizar.disco.simulation.simulators.SimulationException;
 
@@ -17,6 +20,10 @@ public class EmptySimulator implements ISimulator {
 
 	private class VoidProcess extends Process {
 
+		InputStream in = IOUtils.toInputStream("");
+		OutputStream out = new ByteArrayOutputStream();
+		InputStream err = IOUtils.toInputStream("");
+		
 		@Override
 		public void destroy() {
 			// TODO Auto-generated method stub
@@ -32,19 +39,19 @@ public class EmptySimulator implements ISimulator {
 		@Override
 		public InputStream getErrorStream() {
 			// TODO Auto-generated method stub
-			return null;
+			return err;
 		}
 
 		@Override
 		public InputStream getInputStream() {
 			// TODO Auto-generated method stub
-			return null;
+			return in;
 		}
 
 		@Override
 		public OutputStream getOutputStream() {
 			// TODO Auto-generated method stub
-			return null;
+			return out;
 		}
 
 		@Override
@@ -59,14 +66,13 @@ public class EmptySimulator implements ISimulator {
 	public Process simulate(String id, List<EObject> analyzableModel, TraceSet traces, Map<String, String> options,
 			IProgressMonitor monitor) throws SimulationException {
 
-		return new VoidProcess();
+		return new VoidProcess();		
 		
 	}
 
 	@Override
 	public ToolResult getToolResult() {
-		// TODO Auto-generated method stub
-		return null;
+		return WnsimFactory.eINSTANCE.createWnsimResult();
 	}
 
 	@Override
